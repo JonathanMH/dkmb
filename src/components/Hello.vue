aea1838e8386902c7aa72fa3285f6c3f0cfed3e4
Jonathan M. Hethey <public@jonathanmh.com>
2016-12-08 00:44:51 +0100

    adds search function

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="hello container">
-    <h1>DFI film database</h1>
-    <div v-for="movie in shared.movies">
+    <h1>Movie Database</h1>
+    <div class="searchField">
+      <label>Search by title or description:
+        <input type="text" id="search" v-model="private.searchString">
+      </label>
+    </div>
+    <div v-for="movie in searchResults">
       <router-link :to="'/film/' + movie.id">{{movie.id}} {{ movie.title }} : {{ movie.title_dk }}</router-link>
     </div>
   </div>
@@ -14,7 +19,47 @@ export default {
   name: 'hello',
   data() {
     return {
-      shared: store.state
+      shared: store.state,
+      private: {
+        searchString: ''
+      }
+    }
+  },
+  computed: {
+    searchResults: function(){
+      console.log('computing results');
+      var movies = store.state.movies;
+      var searchString = this.private.searchString;
+      console.log(this.private.searchString);
+
+      if(searchString.length == 0){
+        return [];
+      }
+
+      // clean up searchString
+      searchString = searchString.trim().toLowerCase();
+
+      var movieResults = movies.filter(function(item){
+        // @TODO: refactor, repetitive
+        if( item.hasOwnProperty('title_dk') ){
+          if(item.title_dk.toLowerCase().indexOf(searchString) !== -1){
+            return item;
+          }
+        }
+        if( item.hasOwnProperty('title') ){
+          if(item.title.toLowerCase().indexOf(searchString) !== -1){
+            return item;
+          }
+        }
+        if( item.hasOwnProperty('summary') && item.summary.hasOwnProperty('String') ){
+          if(item.summary.String.toLowerCase().indexOf(searchString) !== -1){
+            return item;
+          }
+        }
+      });
+
+      // Return an array with the filtered data.
+      return movieResults;
     }
   },
   created: function(){
